119f1f348c615a6400c43937b1f06be33d507830
Choudhary <ganesh.a.choudhary@capgemini.com>
2026-01-30 15:15:22 +0530

    first

---
+++ 1-MovieTicketBooking/src/main/java/com/cg/exception/ErrorDetails.java
@@ -0,0 +1,29 @@
+package com.cg.exception;
+
+import java.util.Date;
+
+public class ErrorDetails {
+	private String message;
+	private Date timestamp;
+	private String details;
+	
+	
+	public ErrorDetails(String message, Date timestamp, String details) {
+		super();
+		this.message = message;
+		this.timestamp = timestamp;
+		this.details = details;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public Date getTimestamp() {
+		return timestamp;
+	}
+	public String getDetails() {
+		return details;
+	}
+	
+	
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# movie-ticket-Project-repo
