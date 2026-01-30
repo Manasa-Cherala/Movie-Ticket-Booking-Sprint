a55ffa865e9a091628ccbfcdafc350b9cef89e11
Choudhary <ganesh.a.choudhary@capgemini.com>
2026-01-30 17:03:46 +0530

    Update entity

--- 1-MovieTicketBooking/src/main/java/com/cg/entity/Movie.java
+++ 1-MovieTicketBooking/src/main/java/com/cg/entity/Movie.java
@@ -7,7 +7,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 @Entity
+@Table(name= "movie")
 public class Movie {
 	   @Id
 	    @GeneratedValue(strategy = GenerationType.IDENTITY)
