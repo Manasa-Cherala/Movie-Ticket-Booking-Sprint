d32ac0f0cc2bca76b6a388ca55d6afe0a34a56b4
Ganeshchoudhary0911 <GANESH.A.CHOUDHARY@CAPGEMINI.COM>
2026-01-30 16:45:03 +0530

    .

--- 1-MovieTicketBooking/src/main/java/com/cg/entity/Movie.java
+++ 1-MovieTicketBooking/src/main/java/com/cg/entity/Movie.java
@@ -22,6 +22,24 @@ public class Movie {
 	 
 	    @OneToMany(mappedBy = "movie")
 	    private List<Show> shows;
+
+	 public Movie() {
+	    	
+	    }
+	    
+	    
+	    public Movie(Long movieId, String movieName, String genre, String language, int duration, String description,
+				double rating, List<Show> shows) {
+			super();
+			this.movieId = movieId;
+			this.movieName = movieName;
+			this.genre = genre;
+			this.language = language;
+			this.duration = duration;
+			this.description = description;
+			this.rating = rating;
+			this.shows = shows;
+		}
 	    
 		public Long getMovieId() {
 			return movieId;
